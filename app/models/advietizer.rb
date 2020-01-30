9ab51d5ec03d7e674ffc4afb15a91eeff03c5257
AlexIv <alx.ekb@gmail.com>
2020-01-30 22:35:41 +0500

    initialize repo

---
+++ app/models/advietizer.rb
@@ -0,0 +1,2 @@
+class Advietizer < User
+end
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'user/index'
+  get 'user/create'
+  get 'user/edit'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
